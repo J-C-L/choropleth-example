29178429ad4648f075069429ba3837265b7b8307
Janice Lichtman <jalichtman@gmail.com>
2017-07-25 09:55:42 -0700

    build choropleth based on example

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/assets/index.html'))
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+
+app.use(express.static('assets'))
